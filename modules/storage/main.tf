12eca190fcb8b27938584e0499a423a320720bad
Eric Smalling <ericsmalling@users.noreply.github.com>
2020-11-17 15:08:31 -0600

    unencrypted ebs

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket" "insecure-bucket" {
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 20
-
+  encrypted         = false
   tags = {
     Name = "insecure"
   }
