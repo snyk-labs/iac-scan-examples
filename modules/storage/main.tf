0430f73e3f80b8d78ff28919080908082f1f071c
Micah Silverman <micah@afitnerd.com>
2023-03-29 08:53:47 -0400

    added storage fix for high issue

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,6 +2,13 @@ resource "aws_s3_bucket" "insecure-bucket" {
   bucket = "insecure-bucket"
 }
 
+# resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 20
